f3e5fc6f4374bef940bce1a92d3c189b8d5be786
seiryuu <draxasyt@gmail.com>
2025-03-18 22:05:50 +0100

    cvic4

--- functions.php
+++ functions.php
@@ -92,5 +92,32 @@ function printMenu(array $menu){
     }
 }
 
+function preparePortfolio(int $numberOfRows = 2, int $numberOfCols = 4): array{
+    $portfolio = [];
+    $colIndex = 1;
+    for ($i = 1; $i <= $numberOfRows; $i++) {
+        for($j = 1; $j <= $numberOfCols; $j++) {
+            $portfolio[$i][$j] = $colIndex;
+            $colIndex++;
+        }
+    }
+    return $portfolio;
+}
+
+function finishPortfolio(){
+    $portfolio = preparePortfolio();
+    foreach ($portfolio as $row => $col) {
+        echo '<div class="row">';
+        foreach ($col as $index) {
+            echo '<div class="col-25 portfolio text-white text-center" id="portfolio-'.$index.'">
+                                        Web stránka '.$index.'
+                                     </div>';
+        }
+        echo '</div>';
+    }
+}
+
+
+
 ?>
 
--- portfolio.php
+++ portfolio.php
@@ -13,34 +13,9 @@ include_once "../sablona/parts/nav.php";
                 </div>
             </section>
               <section class="container">
-                <div class="row">
-                  <div class="col-25 portfolio text-white text-center" id="portfolio-1">
-                      Web stránka 1
-                  </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-2">
-                        Web stránka 2
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-3">
-                        Web stránka 3
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-4">
-                        Web stránka 4
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-5">
-                        Web stránka 5
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-6">
-                        Web stránka 6
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-7">
-                        Web stránka 7
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-8">
-                        Web stránka 8
-                    </div>
-                </div>
+                  <?php
+                  finishPortfolio();
+                  ?>
             </section>   
 
         </main>
